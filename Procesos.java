8aa9749b0f09448703d5b1e847d09a866cb56a0b
Sandro Córdova <33694187+sandrocordova@users.noreply.github.com>
2017-12-04 18:48:57 -0500

    Add files via upload

---
+++ Procesos.java
@@ -0,0 +1,38 @@
+
+package procesos;
+
+public class Procesos {
+    private static String[] preguntas=new String[10];
+
+    public static String[] getPreguntas() {
+        return preguntas;
+    }
+
+    public static int[] getRespuestas() {
+        return respuestas;
+    }
+     private static int[] respuestas=new int [10];
+    public static void generarPreguntaRespuestas(){
+        PreguntaAdicion adicion;
+        PreguntaResta resta;
+        PreguntaMultiplicacion multiplicacion= new PreguntaMultiplicacion();
+        
+        int cont=0;
+            preguntas[cont]=multiplicacion.getPregunta();
+            respuestas[cont]=multiplicacion.getRespuestaCorrecta();
+        while (cont<9) {
+            cont++;
+            adicion = new PreguntaAdicion();
+            preguntas[cont]=adicion.getPregunta();
+            respuestas[cont]=adicion.getRespuestaCorrecta();
+            cont++;
+            resta = new PreguntaResta();
+            preguntas[cont]=resta.getPregunta();
+            respuestas[cont]=resta.getRespuestaCorrecta();
+            cont++;
+            multiplicacion = new PreguntaMultiplicacion();
+            preguntas[cont]=multiplicacion.getPregunta();
+            respuestas[cont]=multiplicacion.getRespuestaCorrecta();
+        }
+    }
+}
