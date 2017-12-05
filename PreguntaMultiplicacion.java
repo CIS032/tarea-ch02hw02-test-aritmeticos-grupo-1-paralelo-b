65a68889e0093e079c4d55c6c231ecf51285f4d1
bryanrequenes17 <33442086+bryanrequenes17@users.noreply.github.com>
2017-12-04 22:00:20 -0500

    Update PreguntaMultiplicacion.java

--- PreguntaMultiplicacion.java
+++ PreguntaMultiplicacion.java
@@ -6,16 +6,17 @@ package procesos;
  * @author USUARIO
  */
 public class PreguntaMultiplicacion implements PreguntaEntera{
+//Multiplicacion
     private int a,b;
-    public PreguntaMultiplicacion() {
+    public PreguntaMultiplicacion() {// Se generan los numeros a multiplicar
         a=(int)(Math.random()*50+1);
         b=(int)(Math.random()*50);
     }
 
-    public String getPregunta() {
+    public String getPregunta() {// Se genera la pregunta
         return "¿Cuanto es "+a+" * " +b+ "?";
     }
-    public int getRespuestaCorrecta() {
+    public int getRespuestaCorrecta() {// Se genera el resultado correcto para comparar la respuesta de usuario
         return a*b;
     }
     
