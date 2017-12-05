7c99fd3630dc474c6349658d2f3469c59900f969
Sandro Córdova <33694187+sandrocordova@users.noreply.github.com>
2017-12-04 19:25:23 -0500

    Update PreguntaAdicion.java

--- PreguntaAdicion.java
+++ PreguntaAdicion.java
@@ -4,21 +4,21 @@
  * and open the template in the editor.
  */
 package procesos;
-
+//Adición
 public class PreguntaAdicion implements PreguntaEntera{
     private int a,b;
-    public PreguntaAdicion() {
+    public PreguntaAdicion() {//aquí se generan los números que a continuación se utilizará en la operación
         a=(int)(Math.random()*50+1);
         b=(int)(Math.random()*50);
     }
-    public  void  prueba(){
+    public  void  prueba(){//String de la pregunta
         System.out.println(a+" + "+b);
     }
     
-     public String getPregunta() {
+     public String getPregunta() {//presentación de la pregunta 
         return "¿Cuanto es "+a+" + " +b+ "?";
     }
-    public int getRespuestaCorrecta() {
+    public int getRespuestaCorrecta() {//calcula y envía la respuesta correcta de la suma 
         return a+b;
     }
 }
